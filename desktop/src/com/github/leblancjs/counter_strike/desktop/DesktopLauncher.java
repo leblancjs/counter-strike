5a0b7db0b65ebd13949e6768d82f3a0e1afbb63f
Joel Leblanc <leblanc.js@icloud.com>
2020-12-17 12:30:18 -0500

    Initial commit

---
+++ desktop/src/com/github/leblancjs/counter_strike/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.leblancjs.counter_strike.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.leblancjs.counter_strike.GameStarter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameStarter(), config);
+	}
+}
---
+++ html/src/com/github/leblancjs/counter_strike/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.github.leblancjs.counter_strike.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.github.leblancjs.counter_strike.GameStarter;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new GameStarter();
+        }
+}
